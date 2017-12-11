2214864400683adde1895f5f06d864862a3fb264
银希琪 <537083314@qq.com>
2017-12-11 16:31:13 +0800

    slider

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,49 @@
 <template>
-  <div>
-    推荐页面
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" >
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+  import Slider from 'base/slider/slider'
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+  export default {
+    data () {
+      return {
+        recommends : []
+      }
+    },
+    created () {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if(res.code === ERR_OK){
+            this.recommends = res.data.slider
+          }
+        })
+      }
+    },
+    components: {
+      Slider
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">

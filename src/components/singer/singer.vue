48a0a343bb043b7f2f795f480886f2a06bc0602a
银希琪 <537083314@qq.com>
2017-12-14 15:52:45 +0800

    listview & singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="singer">
-
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import ListView from 'base/listview/listview'
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
   export default {
     data() {
       return {
@@ -20,13 +26,56 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if(res.code === ERR_OK){
-            this.singers = res.data.list
+            this.singers = this._normalizeSinger(res.data.list)
           }   
         })
       },
       _normalizeSinger(list) {
-        
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid           
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        //修改成有序列表，需要处理 map
+        let hot =[]
+        let ret =[]
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          }else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        //给 ret 做升序处理
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
